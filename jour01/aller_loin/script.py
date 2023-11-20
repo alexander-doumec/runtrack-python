fc47404ef647da3d1401ce6576b0240ffb1b8129
alexander-doumec <alexander.doumec@laplateforme.io>
2023-11-20 16:02:53 +0100

    python

---
+++ jour01/aller_loin/script.py
@@ -0,0 +1,8 @@
+# Demander à l'utilisateur de saisir une chaîne de caractères
+chaine = input("Entrez une chaîne de caractères : ")
+
+# Vérifier si la chaîne contient le caractère 'e'
+if 'e' in chaine:
+    print("La chaîne contient le caractère 'e'.")
+else:
+    print("La chaîne ne contient pas le caractère 'e'.")
